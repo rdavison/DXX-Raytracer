d1920aa130fa439100f3db5f66276c1c4d109119
Richard Davison <richard.neil.davison@gmail.com>
2026-02-02 20:26:39 -0500

    Step 5.5: d1/include/gr.h — Add RT_METAL to bitmap struct

--- d1/include/gr.h
+++ d1/include/gr.h
@@ -64,7 +64,7 @@ typedef struct _grs_point {
 #define BM_SVGA     2
 #define BM_RGB15    3   //5 bits each r,g,b stored at 16 bits
 #define BM_SVGA15   4
-#if defined(OGL) || defined(RT_DX12)
+#if defined(OGL) || defined(RT_DX12) || defined(RT_METAL)
 #define BM_OGL      5
 #define BM_RGBA8    6
 #endif /* def OGL */
@@ -98,7 +98,7 @@ typedef struct _grs_bitmap {
 	fix avg_color_rgb[3]; // same as above but real rgb value to be used to textured objects that should emit light
 	sbyte   unused;     // to 4-byte align.
 	struct _grs_bitmap  *bm_parent;
-#ifdef RT_DX12
+#if defined(RT_DX12) || defined(RT_METAL)
 	struct _dx_texture* dxtexture;
 #endif
 #ifdef OGL
@@ -119,7 +119,7 @@ typedef struct _grs_font {
 	ubyte    ** ft_chars;       // Ptrs to data for each char (required for prop font)
 	short     * ft_widths;      // Array of widths (required for prop font)
 	ubyte     * ft_kerndata;    // Array of kerning triplet data
-#if defined(OGL) || defined(RT_DX12)
+#if defined(OGL) || defined(RT_DX12) || defined(RT_METAL)
 	// These fields do not participate in disk i/o!
 	grs_bitmap *ft_bitmaps;
 	grs_bitmap ft_parent_bitmap;
