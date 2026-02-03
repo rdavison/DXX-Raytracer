fe092d35173b12f926ba53325d0e6f333e991486
Richard Davison <richard.neil.davison@gmail.com>
2026-02-02 19:53:45 -0500

    Step 1.2: Add macOS error/timer impl

--- RT/Core/Common.c
+++ RT/Core/Common.c
@@ -1,8 +1,16 @@
+#ifdef _WIN32
 #include <Windows.h>
 #include <wchar.h>
 #include <Shlwapi.h>
 
 #pragma comment(lib, "shlwapi")
+#elif defined(__APPLE__)
+#include <libgen.h>
+#include <mach/mach_time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#endif
 
 #include "Core/Common.h"
 #include "Core/Arena.h"
@@ -12,6 +20,7 @@
 
 void RT_FATAL_ERROR_(const char *explanation, const char *title, const char *file, int line)
 {
+#ifdef _WIN32
     char file_stripped[256];
     RT_SaneStrncpy(file_stripped, file, RT_ARRAY_COUNT(file_stripped));
 
@@ -23,22 +32,43 @@ void RT_FATAL_ERROR_(const char *explanation, const char *title, const char *fil
     __debugbreak(); // If you're using a debugger, now is your chance to debug.
 
     ExitProcess(1); // goodbye forever
+#elif defined(__APPLE__)
+    char file_stripped[256];
+    RT_SaneStrncpy(file_stripped, file, RT_ARRAY_COUNT(file_stripped));
+
+    const char *base = basename(file_stripped);
+    char *message = RT_ArenaPrintF(&g_thread_arena, "%s\n\nLocation:\n%s:%d\n\nExplanation:\n%s\n\n" SUPPORT_STRING, title, base, line, explanation);
+    fprintf(stderr, "%s\n", message);
+
+    __builtin_trap(); // If you're using a debugger, now is your chance to debug.
+
+    exit(1); // goodbye forever
+#endif
 }
 
+#ifdef _WIN32
 static LARGE_INTEGER perf_freq;
+#endif
 
 RT_HighResTime RT_GetHighResTime(void)
 {
+#ifdef _WIN32
     LARGE_INTEGER value;
     QueryPerformanceCounter(&value);
 
     RT_HighResTime result;
     result.value = value.QuadPart;
     return result;
+#elif defined(__APPLE__)
+    RT_HighResTime result;
+    result.value = (int64_t)mach_absolute_time();
+    return result;
+#endif
 }
 
 double RT_SecondsElapsed(RT_HighResTime start, RT_HighResTime end)
 {
+#ifdef _WIN32
     if (!perf_freq.QuadPart)
     {
         QueryPerformanceFrequency(&perf_freq);
@@ -46,4 +76,15 @@ double RT_SecondsElapsed(RT_HighResTime start, RT_HighResTime end)
 
     double result = (double)(end.value - start.value) / (double)perf_freq.QuadPart;
     return result;
+#elif defined(__APPLE__)
+    static mach_timebase_info_data_t timebase;
+    if (timebase.denom == 0)
+    {
+        mach_timebase_info(&timebase);
+    }
+
+    uint64_t delta = (uint64_t)(end.value - start.value);
+    double nanos = (double)delta * (double)timebase.numer / (double)timebase.denom;
+    return nanos / 1e9;
+#endif
 }
